a5484c4b0ad3d4a7e5b79f903059066afcaf1d94
Dylan Applegate <dean.brophy@gmail.com>
2025-10-11 17:40:41 -0600

    Refactor part1 and part2 functions for improved readability; streamline movement logic and ensure consistent direction handling

--- 2016/day01/python/solution.py
+++ 2016/day01/python/solution.py
@@ -6,6 +6,33 @@ def part1(input_data):
 
     moves = input_data.split(', ')
 
+    for move in moves:
+        if move[0] == 'R':
+            direction = (direction + 1) % 4
+        elif move[0] == 'L':
+            direction = (direction - 1 + 4) % 4
+
+        distance = int(move[1:])
+        dx, dy = directions[direction]
+
+        x += dx * distance
+        y += dy * distance
+
+    return abs(x) + abs(y)
+
+
+
+
+
+
+def part2(input_data):
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+    x, y = 0, 0
+    direction = 0
+
+    moves = input_data.split(', ')
+
     seen = {(x, y)}
 
     for move in moves:
@@ -17,18 +44,13 @@ def part1(input_data):
         distance = int(move[1:])
         dx, dy = directions[direction]
 
-        for i in range(distance):
+        for _ in range(distance):
             x += dx
             y += dy
 
             if((x, y) in seen):
                 return abs(x) + abs(y)
             seen.add((x, y))
+
+
     return None
-
-
-
-
-
-def part2(input_data):
-    pass
