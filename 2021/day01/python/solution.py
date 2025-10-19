97e75392171d73b761641278ee0b87a5303185f1
Dylan Applegate <dean.brophy@gmail.com>
2025-10-19 13:14:18 -0600

    Add initial implementations for Day 1 solutions in Python and JavaScript; include example input files and test data structure

---
+++ 2021/day01/python/solution.py
@@ -0,0 +1,12 @@
+def format_input(input: str):
+    return [ int(line.strip()) for line in input.strip().split("\n")]
+
+def part1(input_data):
+    input_data = format_input(input_data)
+    greater_than = sum([1 for prev, current in zip(input_data, input_data[1:]) if current > prev])
+    return str(greater_than)
+
+def part2(input_data):
+    input_data = format_input(input_data)
+    greater_than = sum([1 for prev, current in zip(input_data, input_data[3:]) if current > prev])
+    return str(greater_than)
