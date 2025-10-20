ba6cf6ceaf4a1e17bab7422ed6255fe0629fe9a5
Dylan Applegate <dean.brophy@gmail.com>
2025-10-19 22:30:41 -0600

    Add input data and implement part 1 and part 2 logic in Python solution

--- 2024/day01/python/solution.py
+++ 2024/day01/python/solution.py
@@ -1,5 +1,28 @@
+import re
+from collections import Counter
+
+
+def get_part1_tuple(row):
+    pattern = r"\d+"
+    digit_strings = re.findall(pattern, row)
+    return (int(digit_strings[0]), int(digit_strings[1]))
+
+
 def part1(input_data):
-    pass
+
+    rows = [get_part1_tuple(row) for row in input_data.strip().split("\n")]
+    left = sorted([values[0] for values in rows])
+    right = sorted([values[1] for values in rows])
+    zipped = [(lValue, rValue) for lValue, rValue in zip(left, right)]
+    diffs = [abs(lValue - rValue) for lValue, rValue in zipped]
+    return sum(diffs)
+
 
 def part2(input_data):
-    pass
+
+    rows = [get_part1_tuple(row) for row in input_data.strip().split("\n")]
+    left = [values[0] for values in rows]
+    right = Counter([values[1] for values in rows])
+    values = [num * right[num] for num in left]
+    print(values)
+    return sum(values)
