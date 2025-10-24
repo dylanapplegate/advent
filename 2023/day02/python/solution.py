08f5bb5831123dd12b096cc14aef397a56c66031
Dylan Applegate <dean.brophy@gmail.com>
2025-10-24 14:25:59 -0600

    Implement game data processing functions for cube counting and validation in part 1

--- 2023/day02/python/solution.py
+++ 2023/day02/python/solution.py
@@ -1,5 +1,38 @@
+def get_cube_maxes(game):
+    cube_count = {}
+    turns = game.strip().split(":")[1].strip().split(";")
+    game_id = int(game.strip().split(":")[0].strip().split(" ")[1])
+
+    for turn in turns:
+        cubes_string = [cube.strip().split(" ") for cube in turn.split(",")]
+        for count, color in cubes_string:
+            count = int(count)
+            cube_count[color] = max(count, cube_count.get(color, 0))
+
+    return game_id, cube_count
+
+
+def format_data(input_data):
+    game_rows = input_data.strip().split("\n")
+    game_dictionaries = [get_cube_maxes(game) for game in game_rows]
+    return game_dictionaries
+
+
+def is_possible(cube_count):
+    on_hand = {"red": 12, "green": 13, "blue": 14}
+
+    for color, count in on_hand.items():
+        if color in cube_count and count < cube_count[color]:
+            return False
+    return True
+
+
 def part1(input_data):
-    pass
+    games = format_data(input_data)
+
+    valid_games = sum([game_id for game_id, game in games if is_possible(game)])
+    return valid_games
+
 
 def part2(input_data):
     pass
