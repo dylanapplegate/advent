4257e27f579a48f4ad2b67f4d0f52ca7d21b69d2
Dylan Applegate <dean.brophy@gmail.com>
2025-10-24 13:32:23 -0600

    Implement initial solutions for Day 02 of 2024: Add JavaScript and Python solutions for part 1, including input formatting and safety checks. Create test data structure for future tests.

---
+++ 2024/day02/javascript/solution.js
@@ -0,0 +1,40 @@
+function formatInputData(data) {
+  return data.split("\n").map((line) => line.split(" ").map(Number));
+}
+
+function isSafe(report) {
+  const diffs = report
+    .slice(0, report.length - 1)
+    .map((level, i) => report[i + 1] - level);
+
+  const ascendingSafe = diffs.every(
+    (diff) => diff > 0 && Math.abs(diff) >= 1 && Math.abs(diff) <= 3,
+  );
+  const descendingSafe = diffs.every(
+    (diff) => diff < 0 && Math.abs(diff) >= 1 && Math.abs(diff) <= 3,
+  );
+
+  return ascendingSafe || descendingSafe;
+}
+
+function isSafeWithException(report) {
+  for (let i = 0; i < report.length; i++) {
+    if (isSafe([...report.slice(0, i), ...report.slice(i + 1)])) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function part1(input) {
+  const reports = formatInputData(input);
+  console.log({ reports });
+  const safeReportCount = reports.filter((report) => isSafe(report)).length;
+  return safeReportCount;
+}
+
+function part2(input) {
+  return undefined;
+}
+
+module.exports = { part1, part2 };
---
+++ 2024/day02/python/solution.py
@@ -0,0 +1,38 @@
+from typing import List
+
+
+def format_input_data(input: str) -> List[List[int]]:
+    lines = [
+        [int(level) for level in line.strip().split(" ")]
+        for line in input.strip().split("\n")
+    ]
+    return lines
+
+
+def is_safe(report: List[int]):
+    diffs = [l_level - r_level for l_level, r_level in zip(report, report[1:])]
+    return all(diff < 0 and abs(diff) >= 1 and abs(diff) <= 3 for diff in diffs) or all(
+        diff > 0 and abs(diff) >= 1 and abs(diff) <= 3 for diff in diffs
+    )
+
+
+def is_safe_with_single_exception(report: List[int]):
+    for i in range(len(report)):
+        if is_safe(report[:i] + report[i + 1 :]):
+            return True
+
+    return False
+
+
+def part1(input_data):
+    reports = format_input_data(input_data)
+    is_safe_reports = [is_safe(report) for report in reports]
+    safe_report_count = len([is_safe for is_safe in is_safe_reports if is_safe])
+    return safe_report_count
+
+
+def part2(input_data):
+    reports = format_input_data(input_data)
+    is_safe_reports = [is_safe_with_single_exception(report) for report in reports]
+    safe_report_count = len([is_safe for is_safe in is_safe_reports if is_safe])
+    return safe_report_count
