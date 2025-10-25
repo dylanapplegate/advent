cfa55974e763d03b52afc322ba92faee07cf4c5e
Dylan Applegate <dean.brophy@gmail.com>
2025-10-25 11:08:58 -0600

    Implement game data processing functions for cube counting and validation in part 1 and part 2

--- 2023/day02/javascript/solution.js
+++ 2023/day02/javascript/solution.js
@@ -1,9 +1,61 @@
+function getCubeMaxes(game) {
+  const cubeCount = {};
+  const [gameString, contentString] = game
+    .trim()
+    .split(":")
+    .map((s) => s.trim());
+  const turns = contentString.split(";").map((s) => s.trim());
+  const gameId = Number(gameString.split(" ")[1]);
+
+  for (const turn of turns) {
+    turn
+      .split(",")
+      .map((cube) => {
+        const [count, color] = cube.trim().split(" ");
+        return [color, count];
+      })
+      .forEach(([color, count]) => {
+        cubeCount[color] = Math.max(cubeCount[color] ?? 0, count);
+      });
+  }
+
+  return [gameId, cubeCount];
+}
+
+function formatData(input) {
+  const gameRows = input.trim().split("\n");
+  const gameDictionaries = gameRows.map((row) => getCubeMaxes(row));
+  return gameDictionaries;
+}
+
+function isPossible(cubeCount) {
+  const onHand = { red: 12, green: 13, blue: 14 };
+
+  for (const [color, count] of Object.entries(onHand)) {
+    if (color in cubeCount && count < cubeCount[color]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function part1(input) {
-  return undefined;
+  const games = formatData(input);
+  const validGames = games
+    .filter(([gameId, game]) => isPossible(game))
+    .map(([gameId, game]) => gameId)
+    .reduce((a, v) => a + v);
+  return validGames;
+}
+
+function powerOfCubes(cubeCount) {
+  return ["red", "blue", "green"].reduce((acc, key) => acc * cubeCount[key], 1);
 }
 
 function part2(input) {
-  return undefined;
+  return formatData(input)
+    .map(([, game]) => powerOfCubes(game))
+    .reduce((acc, gameId) => acc + gameId);
 }
 
 module.exports = { part1, part2 };
