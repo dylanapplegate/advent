29ae940cb5ac8b31eb9079458551926ed04168f9
Dylan Applegate <dean.brophy@gmail.com>
2025-10-29 22:07:19 -0600

    refactor

--- src/2016/day03/python/solution.py
+++ src/2016/day03/python/solution.py
@@ -6,32 +6,26 @@ def format_data(data: str) -> list[tuple[int, int, int]]:
     return number_lines
 
 
+def is_valid_triangle(a: int, b: int, c: int) -> bool:
+    return a + b > c and a + c > b and b + c > a
+
+
 def part1(input: str) -> int:
-    return len(
-        [
-            True
-            for side1, side2, side3 in format_data(input)
-            if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1
-        ]
-    )
+    return sum(is_valid_triangle(a, b, c) for a, b, c in format_data(input))
 
 
 def part2(input: str) -> int:
     matrix = format_data(input)
     valid_triangles: int = 0
 
+    all_sides = [side for triangle in matrix for side in triangle]
+
     for c in range(0, 3):
-        for r in range(0, len(matrix), 3):
-            side1 = matrix[r + 0][c]
-            side2 = matrix[r + 1][c]
-            side3 = matrix[r + 2][c]
-
-            is_valid = (
-                side1 + side2 > side3
-                and side1 + side3 > side2
-                and side2 + side3 > side1
-            )
-
-            valid_triangles += 1 if is_valid else 0
+        valid_triangles += sum(
+            [
+                is_valid_triangle(all_sides[i], all_sides[i + 3], all_sides[i + 6])
+                for i in range(c, len(all_sides), 9)
+            ]
+        )
 
     return valid_triangles
